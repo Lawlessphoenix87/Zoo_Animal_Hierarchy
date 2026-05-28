3589d3ce7626a482ce6a16aadb06638187ac64c8
Lawlessphoenix87 <87556244+Lawlessphoenix87@users.noreply.github.com>
2026-05-27 21:27:43 -0700

    Initial commit
    Created repo and java project.  Created the base abstract Animal class with fields constructor and other methods

---
+++ Zoo Animal Hierarchy/src/Animal.java
@@ -0,0 +1,35 @@
+//Keenan Roberts
+// Date: 5/27/2026
+// Assignment: Assignment 11 - Zoo Animal Hierarchy
+
+public abstract class Animal 
+{
+    protected String _name;
+    protected int _age;
+    protected double _weight;
+
+    //Constructor
+    public Animal(String name, int age, double weight)
+    {
+        _name = name;
+        _age = age;
+        _weight = weight;
+    }
+
+    //Accessors
+    public String GetName() {return _name;}
+    public int GetAge() {return _age;}
+    public double GetWeight() {return _weight;}
+
+
+    // ======== METHOD LIBRARY ========
+    public String describe()
+    {
+        return getClass().getSimpleName() + ":" + _name + ", age " + _age 
+            + ", " + _weight + " kg";
+    }
+
+    public abstract String makeSound();
+    public abstract String getFavoriteFood();
+}
+
